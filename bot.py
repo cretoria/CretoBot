6a01c18c34f44495f65a30bc5bb32a865d5789e8
Cretoria <39278094+cretoria@users.noreply.github.com>
2018-07-19 15:41:05 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,18 +5,22 @@ import json
 import aiohttp
 import time
 import config
-
-
-
 from discord.ext import commands
 
-client = discord.Client()
+bot = discord.Client()
+bot = commands.Bot(command_prefix='+', description='A bot that greets the user back.')
 
-@client.event
+@bot.event
 async def on_ready():
     print ('Logged in as')
-    print (client.user.name)
-    print (client.user.id)
+    print (bot.user.name)
+    print (bot.user.id)
     print ('------')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello there! :wave:")
+    
+
 
 client.run(config.token)
